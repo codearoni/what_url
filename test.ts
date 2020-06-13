d4aa1a791e5c2c6cb29428a67386ab9cfdc8fad9
codearoni <devnull@codearoni.io>
2020-06-13 01:02:21 -0500

    Additional calculated params

--- test.ts
+++ test.ts
@@ -1,53 +1,162 @@
-import { assertEquals, assertThrows } from "https://deno.land/std/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std/testing/asserts.ts";
 import { WhatUrl } from "./what_url.ts";
 
 Deno.test({
-    name: 'Returns correct origin given a protocol',
-    fn(): void {
-        const url = new WhatUrl()
-            .setProtocol('https:')
-            .build();
-        
-        assertEquals(url.getOrigin(), 'https://');
-    }
+  name: "Returns correct origin given a protocol",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .build();
+
+    assertEquals(url.origin, "https://");
+    console.log(url.protocol);
+  },
 });
 
 Deno.test({
-    name: 'Creates a fully populated url',
-    fn(): void {
-        const url = new WhatUrl()
-            .setProtocol('https:')
-            .setUsername('what')
-            .setPassword('1234')
-            .setHostname('deno.land')
-            .setPort(8080)
-            .setPathname('path/to/file')
-            .addParam('x', 'hello_world')
-            .addParam('y', 2)
-            .addParam('z', true)
-            .setHash('asdf')
-            .build();
+  name: "Returns an auth with no password",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setHostname("deno.land")
+      .setPort(8080)
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
 
-        assertEquals(url.toString(), 'https://what:1234@deno.land:8080?x=hello_world&y=2&z=true#asdf');
-    }
+    assertEquals(url.auth, url.username);
+  },
 });
 
 Deno.test({
-    name: 'Parses a fully populated url',
-    fn(): void {
-        console.log(new URL('https://what:1234@deno.land:8080?x=hello_world&y=&z=true#asdf'));
-        const url = new WhatUrl('https://what:1234@deno.land:8080?x=hello_world&y=&z=true#asdf')
+  name: "Returns an auth with a password",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setPassword("1234")
+      .setHostname("deno.land")
+      .setPort(8080)
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
 
-        console.log(url);
-    }
+    assertEquals(url.auth, url.username + ":" + url.password);
+  },
 });
 
 Deno.test({
-    name: 'Parses a url',
-    fn(): void {
-        console.log(new URL('https://what:1234@deno.land:8080#asdf'));
-        const url = new WhatUrl('https://what:1234@deno.land:8080#asdf')
+  name: "Returns a host without a port",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setPassword("1234")
+      .setHostname("deno.land")
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
 
-        console.log(url);
-    }
+    assertEquals(url.host, url.hostname);
+  },
+});
+
+Deno.test({
+  name: "Returns a host with a port",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setPassword("1234")
+      .setHostname("deno.land")
+      .setPort(8080)
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
+
+    assertEquals(url.host, url.hostname + ":" + url.port);
+  },
+});
+
+Deno.test({
+  name: "Returns a hash",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setPassword("1234")
+      .setHostname("deno.land")
+      .setPort(8080)
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
+
+    assertEquals(url.hash, "asdf");
+  },
+});
+
+Deno.test({
+  name: "Creates a fully populated url",
+  fn(): void {
+    const url = new WhatUrl()
+      .setProtocol("https:")
+      .setUsername("what")
+      .setPassword("1234")
+      .setHostname("deno.land")
+      .setPort(8080)
+      .setPathname("path/to/file")
+      .addParam("x", "hello_world")
+      .addParam("y", 2)
+      .addParam("z", true)
+      .setHash("asdf")
+      .build();
+
+    assertEquals(
+      url.toString(),
+      "https://what:1234@deno.land:8080?x=hello_world&y=2&z=true#asdf",
+    );
+  },
+});
+
+Deno.test({
+  name: "Parses a fully populated url",
+  fn(): void {
+    console.log(
+      new URL("https://what:1234@deno.land:8080?x=hello_world&y=&z=true#asdf"),
+    );
+    const url = new WhatUrl(
+      "https://what:1234@deno.land:8080?x=hello_world&y=&z=true#asdf",
+    );
+
+    console.log(url);
+  },
+});
+
+Deno.test({
+  name: "Parses a url",
+  fn(): void {
+    console.log(new URL("https://what:1234@deno.land:8080#asdf"));
+    const url = new WhatUrl("https://what:1234@deno.land:8080#asdf");
+
+    console.log(url);
+  },
 });
--- what_url.ts
+++ what_url.ts
@@ -1,217 +1,222 @@
-
 type IQueryParam = string | number | boolean | null;
-type IQuery = Map<string, IQueryParam>
+type IQuery = Map<string, IQueryParam>;
 
 class WhatUrl {
-    private _protoSuffix = '//';
-    readonly protocol: string;
-    readonly username: string;
-    readonly password: string;
-    readonly hostname: string;
-    readonly port: number | null;
-    readonly pathname: string;
-    readonly query: IQuery;
-    readonly hash: string;
-    constructor(whatUrlBuilder: WhatUrlBuilder) {
-        this.protocol = whatUrlBuilder.protocol;
-        this.username = whatUrlBuilder.username;
-        this.password = whatUrlBuilder.password;
-        this.hostname = whatUrlBuilder.hostname;
-        this.port = whatUrlBuilder.port;
-        this.pathname = whatUrlBuilder.pathname;
-        this.query = whatUrlBuilder.query;
-        this.hash = whatUrlBuilder.hash;
+  // component parameters
+  private _protoSuffix = "//";
+  readonly protocol: string;
+  readonly username: string;
+  readonly password: string;
+  readonly hostname: string;
+  readonly port: number | null;
+  readonly pathname: string;
+  readonly query: IQuery;
+  readonly hash: string;
+  // calculated parameters
+  readonly origin: string;
+  readonly auth: string;
+  readonly host: string;
+  constructor(whatUrlBuilder: WhatUrlBuilder) {
+    // component parameters
+    this.protocol = whatUrlBuilder.protocol;
+    this.username = whatUrlBuilder.username;
+    this.password = whatUrlBuilder.password;
+    this.hostname = whatUrlBuilder.hostname;
+    this.port = whatUrlBuilder.port;
+    this.pathname = whatUrlBuilder.pathname;
+    this.query = whatUrlBuilder.query;
+    this.hash = whatUrlBuilder.hash;
+    // calculated parameters
+    this.origin = this.protocol + this._protoSuffix;
+    this.auth = this.username + (this.password ? ":" + this.password : "");
+    this.host = this.hostname + (this.port ? ":" + this.port : "");
+  }
+  getParam(key: string) {
+    return this.query.get(key);
+  }
+  toString(): string {
+    let urlStr = "";
+
+    if (this.protocol) {
+      urlStr = this.protocol + this._protoSuffix;
     }
-    getOrigin() {
-        return this.protocol + this._protoSuffix;
+
+    if (this.username) {
+      urlStr = urlStr + this.username;
+
+      if (this.password) {
+        urlStr = urlStr + ":" + this.password;
+      }
+
+      urlStr = urlStr + "@";
     }
-    getParam(key: string) {
-        return this.query.get(key);
+
+    if (this.hostname) {
+      urlStr = urlStr + this.hostname;
     }
-    toString(): string {
-        let urlStr = '';
-    
-        if (this.protocol) {
-            urlStr = this.protocol + this._protoSuffix;
-        }
 
-        if (this.username) {
-            urlStr = urlStr + this.username;
-
-            if (this.password) {
-                urlStr = urlStr + ':' + this.password;
-            }
-
-            urlStr = urlStr + '@';
-        }
-
-        if (this.hostname) {
-            urlStr = urlStr + this.hostname;
-        }
-
-        if (this.port) {
-            urlStr = urlStr + ':' + this.port;
-        }
-
-        if (this.query.size > 0) {
-            const paramArr: Array<string> = [];
-            urlStr = urlStr + '?';
-            this.query.forEach((value: IQueryParam, key: string) => {
-                if (value === null) {
-                    paramArr.push(key + '=');
-                } else {
-                    paramArr.push(key + '=' + value);
-                }
-            });
-            urlStr = urlStr + paramArr.join('&');
-        }
-
-        if (this.hash) {
-            urlStr = urlStr + '#' + this.hash;
-        }
-
-        return urlStr;
+    if (this.port) {
+      urlStr = urlStr + ":" + this.port;
     }
+
+    if (this.query.size > 0) {
+      const paramArr: Array<string> = [];
+      urlStr = urlStr + "?";
+      this.query.forEach((value: IQueryParam, key: string) => {
+        if (value === null) {
+          paramArr.push(key + "=");
+        } else {
+          paramArr.push(key + "=" + value);
+        }
+      });
+      urlStr = urlStr + paramArr.join("&");
+    }
+
+    if (this.hash) {
+      urlStr = urlStr + "#" + this.hash;
+    }
+
+    return urlStr;
+  }
 }
 
 const parseQueryString = function (qs: string): IQuery {
-    if (qs.length <= 1) {
-        return new Map<string, IQueryParam>();
-    }
-    const qsMap = new Map<string, IQueryParam>();
-
-    const queryString = qs.startsWith('?') ? qs.substring(1) : qs;
-    const kvPairs = queryString.split('&');
-    
-    kvPairs.forEach((pair) => {
-        const entry = pair.split('=');
-        qsMap.set(entry[0], entry[1] || null);
-    });
-
+  const qsMap = new Map<string, IQueryParam>();
+  if (qs.length <= 1) {
     return qsMap;
+  }
+
+  const queryString = qs.startsWith("?") ? qs.substring(1) : qs;
+  const kvPairs = queryString.split("&");
+
+  kvPairs.forEach((pair) => {
+    const entry = pair.split("=");
+    qsMap.set(entry[0], entry[1] || null);
+  });
+
+  return qsMap;
 };
 
 class WhatUrlBuilder {
+  private _protocol: string = "";
+  private _username: string = "";
+  private _password: string = "";
+  private _hostname: string = "";
+  private _port: number | null = null;
+  private _pathname: string = "";
+  private _query: IQuery = new Map();
+  private _hash: string = "";
 
-    private _protocol: string = '';
-    private _username: string = '';
-    private _password: string = '';
-    private _hostname: string = '';
-    private _port: number | null = null;
-    private _pathname: string = '';
-    private _query: IQuery = new Map();
-    private _hash: string = '';
-
-    constructor(whatUrl?: WhatUrl | string) {
-        if (whatUrl instanceof WhatUrl) {
-            this._protocol = whatUrl.protocol;
-            this._username = whatUrl.username;
-            this._password = whatUrl.password;
-            this._hostname = whatUrl.hostname;
-            this._port = whatUrl.port;
-            this._pathname = whatUrl.pathname;
-            this._query = whatUrl.query;
-            this._hash = whatUrl.hash;
-        } else if (typeof whatUrl === 'string') {
-            const parsedUrl = new URL(whatUrl);
-            this._protocol = parsedUrl.protocol;
-            this._username = parsedUrl.username;
-            this._password = parsedUrl.password;
-            this._hostname = parsedUrl.hostname;
-            this._port = parseInt(parsedUrl.port, 10);
-            this._pathname = parsedUrl.pathname;
-            this._query = parseQueryString(parsedUrl.search);
-            this._hash = parsedUrl.hash;
-        }
+  constructor(whatUrl?: WhatUrl | string) {
+    if (whatUrl instanceof WhatUrl) {
+      this._protocol = whatUrl.protocol;
+      this._username = whatUrl.username;
+      this._password = whatUrl.password;
+      this._hostname = whatUrl.hostname;
+      this._port = whatUrl.port;
+      this._pathname = whatUrl.pathname;
+      this._query = whatUrl.query;
+      this._hash = whatUrl.hash;
+    } else if (typeof whatUrl === "string") {
+      const parsedUrl = new URL(whatUrl);
+      this._protocol = parsedUrl.protocol;
+      this._username = parsedUrl.username;
+      this._password = parsedUrl.password;
+      this._hostname = parsedUrl.hostname;
+      this._port = parseInt(parsedUrl.port, 10);
+      this._pathname = parsedUrl.pathname;
+      this._query = parseQueryString(parsedUrl.search);
+      this._hash = parsedUrl.hash;
     }
+  }
 
-    addParam(key: string, value: IQueryParam) {
-        this._query.set(key, value);
-        return this;
-    }
+  addParam(key: string, value: IQueryParam) {
+    this._query.set(key, value);
+    return this;
+  }
 
-    removeParam(key: string) {
-        if(this._query.get(key)) {
-            this._query.delete(key);
-        }
-        return this;
+  removeParam(key: string) {
+    if (this._query.get(key)) {
+      this._query.delete(key);
     }
+    return this;
+  }
 
-    setProtocol(protocol: string) {
-        this._protocol = protocol;
-        return this;
-    }
+  setProtocol(protocol: string) {
+    this._protocol = protocol;
+    return this;
+  }
 
-    setUsername(username: string) {
-        this._username = username;
-        return this;
-    }
+  setUsername(username: string) {
+    this._username = username;
+    return this;
+  }
 
-    setPassword(password: string) {
-        this._password = password;
-        return this;
-    }
+  setPassword(password: string) {
+    this._password = password;
+    return this;
+  }
 
-    setHostname(hostname: string) {
-        this._hostname = hostname;
-        return this;
-    }
+  setHostname(hostname: string) {
+    this._hostname = hostname;
+    return this;
+  }
 
-    setPort(port: number) {
-        this._port = port;
-        return this;
-    }
+  setPort(port: number) {
+    this._port = port;
+    return this;
+  }
 
-    setPathname(pathname: string) {
-        this._pathname = pathname;
-        return this;
-    }
+  setPathname(pathname: string) {
+    this._pathname = pathname;
+    return this;
+  }
 
-    setQuery(query: IQuery) {
-        this._query = query;
-        return this;
-    }
+  setQuery(query: IQuery) {
+    this._query = query;
+    return this;
+  }
 
-    setHash(hash: string) {
-        this._hash = hash;
-        return this;
-    }
+  setHash(hash: string) {
+    this._hash = hash;
+    return this;
+  }
 
-    build() {
-        return new WhatUrl(this);
-    }
+  build() {
+    return new WhatUrl(this);
+  }
 
-    get protocol() {
-        return this._protocol;
-    }
+  get protocol() {
+    return this._protocol;
+  }
 
-    get username() {
-        return this._username;
-    }
+  get username() {
+    return this._username;
+  }
 
-    get password() {
-        return this._password;
-    }
+  get password() {
+    return this._password;
+  }
 
-    get hostname() {
-        return this._hostname;
-    }
+  get hostname() {
+    return this._hostname;
+  }
 
-    get port() {
-        return this._port;
-    }
+  get port() {
+    return this._port;
+  }
 
-    get pathname() {
-        return this._pathname;
-    }
+  get pathname() {
+    return this._pathname;
+  }
 
-    get query() {
-        return this._query;
-    }
+  get query() {
+    return this._query;
+  }
 
-    get hash() {
-        return this._hash;
-    }
+  get hash() {
+    return this._hash;
+  }
 }
 
-export { WhatUrlBuilder as WhatUrl }
+export { WhatUrlBuilder as WhatUrl };
